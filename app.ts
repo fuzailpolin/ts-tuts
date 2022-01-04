5c5b550cfafe45ee985d5da5d0c4d17d82f21fcd
fuzailpolin <fuzail.polin@gmail.com>
2022-01-04 12:39:56 +0600

    feat: understanding the data types

--- app.ts
+++ app.ts
@@ -1,1 +1,21 @@
-console.log("this is ts file...");
+const add = (
+    n1: number,
+    n2: number,
+    printIt: boolean,
+    resultString: string
+) => {
+    let result = n1 + n2;
+    if (printIt) {
+        console.log(`${resultString}: ${result}`);
+    } else {
+        return n1 + n2;
+    }
+};
+
+const number1 = "5";
+const number2 = 3;
+const printThis = true;
+
+add(+number1, +number2, printThis, "The result is");
+const result = add(number2, 4, false, "");
+console.log(result, typeof result);
